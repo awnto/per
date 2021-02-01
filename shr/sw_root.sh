436a11c4f3e7bfa621c13f257b7d21a7af6cbeaf
Rishabh Sarswa <38378349+awnto@users.noreply.github.com>
2021-02-01 21:40:07 +0530

    Update sw_root.sh

--- shr/sw_root.sh
+++ shr/sw_root.sh
@@ -5,6 +5,7 @@ unset LD_PRELOAD
 
 export HOME=/root
 export SHELL=/bin/bash
-proot -0 -r debian -w /root -b /dev -b /sys -b /proc -b /sdcard:/awnto/sdcard /bin/bash
+export PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin
+/data/data/com.termux/files/usr/bin/proot -0 -r debian -w /root -b /dev -b /sys -b /proc -b /sdcard:/awnto/sdcard /bin/bash
 
 
